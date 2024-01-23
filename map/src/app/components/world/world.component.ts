57bf10efdc0ea4613acc8ef924fc37e68be36518
FadingOven <bradyhalsten@gmail.com>
2024-01-22 16:36:15 -0800

    preparing to write js for event listeners

--- map/src/app/components/world/world.component.ts
+++ map/src/app/components/world/world.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-world',
   templateUrl: './world.component.html',
   styleUrl: './world.component.css'
 })
-export class WorldComponent {
-
+export class WorldComponent implements OnInit{
+ngOnInit(): void {
+  //create mouse event listeners for path
+  
+}
 }
