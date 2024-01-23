5c438be77b28a1e25ed7b065d9bc00fa6999aa90
FadingOven <bradyhalsten@gmail.com>
2024-01-23 13:48:42 -0800

    finished script to populate country data

--- map/src/app/components/world/world.component.ts
+++ map/src/app/components/world/world.component.ts
@@ -35,16 +35,19 @@ export class WorldComponent implements OnInit{
     let data: any = await res.json();
     let dataPath:any = data[1];
 
+    console.log('Data Path:', dataPath); // Log the dataPath object to the console
+
+
     let name: string = dataPath[0].name;
     document.getElementById('name')!.innerText = name;
 
-    let capital: string = dataPath[0].capital;
+    let capital: string = dataPath[0].capitalCity;
     document.getElementById('capital')!.innerText = capital;
 
-    let region: string = dataPath[0].region;
+    let region: string = dataPath[0].region.value;
     document.getElementById('region')!.innerText = region;
 
-    let income: string = dataPath[0].incomeLevel;
+    let income: string = dataPath[0].incomeLevel.value;
     document.getElementById('income')!.innerText = income;
 
     let latitude: string = dataPath[0].latitude;
